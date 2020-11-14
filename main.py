facd7d879778094b9492982b01a8a352f70c04c2
Chinmay Gawde <gawdechinmay@gmail.com>
2020-11-14 23:20:31 +0530

    Made changes

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+@app.route('/', methods=["POST"])
+def accept_input():
+    company_name = request.form['company-name']
+    investment_amt = request.form['investment-amt']
+    print(company_name, investment_amt)
+    return render_template("result.html")
     
 @app.route('/news')
 def news():
