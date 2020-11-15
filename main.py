b5d5938402a768ca051ae2cb962ecbc030691e9c
jxswin <ptjeswin99@gmail.com>
2020-11-15 14:09:17 +0530

    added dynamic graph
    just graph and some style change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template,json
 
 app = Flask(__name__)
+
+xdata = [20, 10, 25, 12, 20, 12, 25]
 
 @app.route('/')
 def home():
@@ -11,10 +13,15 @@ def accept_input():
     company_name = request.form['company-name']
     investment_amt = request.form['investment-amt']
     print(company_name, investment_amt)
-    return render_template("result.html")
+    return render_template("result.html" , xdata = json.dumps(xdata))
     
 @app.route('/news')
 def news():
     return render_template("news.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 app.run(debug=True)
